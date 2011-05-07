4efdcfd3e80297292b61b38e0173527ad9ae9b82
Andrei Formiga <archimedes_siracusa@hotmail.com>
2011-05-07 18:44:27 -0300

    Added a top-level interpreter.

---
+++ proplogtop1/TopLevel.scala
@@ -0,0 +1,19 @@
+
+//
+// TopLevel.scala
+// A top-level CLI interface for the Proplog interpreter
+//
+// Andrei de A. Formiga, 2011-05-04
+//
+
+package proplog
+
+object TopLevel {
+  def main(args: Array[String]) {
+    println("Banco de dados: ")
+    // load file
+    // parse
+    // wait input
+    // pass query to interpreter and show results
+  } 
+}
