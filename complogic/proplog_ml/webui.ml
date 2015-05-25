662c8e10fcb819934870e99d8df18ce0063a318f
Andrei Formiga <archimedes_siracusa@hotmail.com>
2015-05-25 19:53:43 -0300

    web interface for inference engine

--- complogic/proplog_ml/webui.ml
+++ complogic/proplog_ml/webui.ml
@@ -17,8 +17,13 @@ let text_input name value =
   let res = doc##createDocumentFragment() in
   Dom.appendChild res (doc##createTextNode(js name));
   let input = Html.createInput ~_type:(js"text") doc in
-  input##value <- js (value);
+  input##value <- js (!value);
   input##size <- 60;
+  input##onchange <- Html.handler
+                       (fun _ -> 
+                        value := Js.to_string input##value;
+                        input##value <- js !value;
+                        Js._false);
   Dom.appendChild res input;
   res
 
@@ -36,6 +41,11 @@ let show_msg msg fb =
 let show_error msg fb = 
   fb##value <- (js @@ "Erro:\n" ^ msg)
 
+let query_results query resp = 
+  let res = if Proplog.establish_goal !base query then "Sim" else "Nao" in
+  let rtx = "?- " ^ query ^ "\n" ^ res in
+  resp##value <- js rtx
+
 let create_feedback_box d = 
   let feedback = Js.Opt.get (d##getElementById(js "feedback")) (fun () -> assert false) in
   let fbtb = Html.createTextarea d in
@@ -45,6 +55,16 @@ let create_feedback_box d =
   Dom.appendChild feedback fbtb;
   Dom.appendChild feedback (Html.createBr d);
   fbtb
+
+let create_response_box d = 
+  let response = Js.Opt.get (d##getElementById(js "response")) (fun () -> assert false) in
+  let rta = Html.createTextarea d in
+  rta##rows <- 8; rta##cols <- 50;
+  Dom.appendChild response (d##createTextNode(js "Resposta:"));
+  Dom.appendChild response (Html.createBr d);
+  Dom.appendChild response rta;
+  Dom.appendChild response (Html.createBr d);
+  rta
   
 let onload _ = 
   let d = Html.document in
@@ -64,18 +84,18 @@ let onload _ =
                                    show_msg "Dados carregados com sucesso." feedback;
                                    Js._false
                                  with Proplog.Parse_error msg -> show_error msg feedback;
+                                                                 base := [];
                                                                  Js._false));
   Dom.appendChild query (Html.createBr d);
   Dom.appendChild query (Html.createBr d);
-  let resp = d##createTextNode (js "") in
-  Dom.appendChild query (text_input "Consulta   ?-" "");
+  let resp = create_response_box d in
+  Dom.appendChild query (text_input "Consulta   ?-" q_text);
   Dom.appendChild query (button "Realizar consulta"
                                 (fun _ ->
+                                 query_results !q_text resp;
                                  Js._false));
   Dom.appendChild query (Html.createBr d);
   Dom.appendChild query (Html.createBr d);
-  Dom.appendChild query (d##createTextNode(js "Resposta:"));
-  Dom.appendChild query resp;
   Js._false
 
 let _ = 
